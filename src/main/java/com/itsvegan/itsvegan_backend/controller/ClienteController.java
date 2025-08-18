2869a93fd82835c35455b8c553bb33366fd40280
andre bovis <andre_bovis@hotmail.com>
2025-08-18 01:24:17 -0300

    feat: Adiciona fluxo de cadastro de cliente

---
+++ src/main/java/com/itsvegan/itsvegan_backend/controller/ClienteController.java
@@ -0,0 +1,28 @@
+package com.itsvegan.itsvegan_backend.controller;
+
+import com.itsvegan.itsvegan_backend.model.Cliente;
+import com.itsvegan.itsvegan_backend.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
