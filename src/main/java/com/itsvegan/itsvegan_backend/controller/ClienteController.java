3016fb312afa322738b2b9505273fc6eac01adad
andre bovis <andre_bovis@hotmail.com>
2025-08-18 02:09:26 -0300

    feat: Conecta o formulário de cadastro com a API

--- src/main/java/com/itsvegan/itsvegan_backend/controller/ClienteController.java
+++ src/main/java/com/itsvegan/itsvegan_backend/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.itsvegan.itsvegan_backend.controller;
 import com.itsvegan.itsvegan_backend.model.Cliente;
 import com.itsvegan.itsvegan_backend.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -24,5 +25,11 @@ public class ClienteController {
     public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
         Optional<Cliente> cliente = clienteRepository.findById(id);
         return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+        Cliente novoCliente = clienteRepository.save(cliente);
+        return new ResponseEntity<>(novoCliente, HttpStatus.CREATED);
     }
 }
