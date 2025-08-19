be5baac4415b63e192eb2d427fb402db0b944307
andre bovis <andre_bovis@hotmail.com>
2025-08-19 02:57:10 -0300

    feat Adicionar endpoints para buscar clientes

--- src/main/java/com/itsvegan/itsvegan_backend/controller/ClienteController.java
+++ src/main/java/com/itsvegan/itsvegan_backend/controller/ClienteController.java
@@ -1,35 +1,28 @@
-package com.itsvegan.itsvegan_backend.controller;
+package com.bancoonline.controller;
 
-import com.itsvegan.itsvegan_backend.model.Cliente;
-import com.itsvegan.itsvegan_backend.repository.ClienteRepository;
+import com.bancoonline.model.Cliente;
+import com.bancoonline.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping
-    public List<Cliente> getAllClientes() {
-        return clienteRepository.findAll();
-    }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
     @PostMapping
     public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
         Cliente novoCliente = clienteRepository.save(cliente);
-        return new ResponseEntity<>(novoCliente, HttpStatus.CREATED);
+        return ResponseEntity.ok(novoCliente);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarCliente(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
