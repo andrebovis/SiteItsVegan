5373d6a84dedc054f47133c3a6799f2f7f317846
andre bovis <andre_bovis@hotmail.com>
2025-08-26 02:30:36 -0300

    feat: Adiciona conteudo para paginas FAQ, Nossa Proposta e Sobre Nos e corrige conflitos de mapeamento de URL

--- src/main/java/com/itsvegan/itsvegan_backend/controller/InstitucionalController.java
+++ src/main/java/com/itsvegan/itsvegan_backend/controller/InstitucionalController.java
@@ -1,4 +1,53 @@
 package com.itsvegan.itsvegan_backend.controller;
 
-import org.springframework.stereotype.Controller;@Controller
-public class InstitucionalController {}
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InstitucionalController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
+
+    @GetMapping("/aniversarios")
+    public String aniversarios() {
+        return "aniversarios.html";
+    }
+
+    @GetMapping("/nossa-proposta")
+    public String nossaProposta() {
+        return "nossaproposta.html";
+    }
+
+    @GetMapping("/cardapio")
+    public String cardapio() {
+        return "cardapio.html";
+    }
+
+    @GetMapping("/armazem")
+    public String armazem() {
+        return "armazem.html";
+    }
+
+    @GetMapping("/carreiras")
+    public String carreiras() {
+        return "carreiras.html";
+    }
+
+    @GetMapping("/faq")
+    public String faq() {
+        return "faq.html";
+    }
+    
+    @GetMapping("/contato")
+    public String contato() {
+        return "contato.html";
+    }
+
+    @GetMapping("/sobrenos")
+    public String sobreNos() {
+        return "sobrenos.html";
+    }
+}
